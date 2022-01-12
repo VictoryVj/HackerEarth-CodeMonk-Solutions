c349fd744e6da5253a688c00e04c9448f5618c28
N VIJAY <67743263+VictoryVj@users.noreply.github.com>
2022-01-12 19:19:36 +0530

    Add files via upload

---
+++ Monk and Rotation.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class TestClass 
+{
+    public static void main(String args[] ) throws Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        int tCount = sc.nextInt();
+        while(tCount>0)
+        {
+            int N = sc.nextInt();
+            int K = sc.nextInt();
+            int A[] = new int[N];
+            for(int loop=0; loop<N; loop++)
+            {
+                A[loop] = sc.nextInt();
+            }
+            K %= N;
+            for(int loop=0; loop<N; loop++)
+            {
+                int result = A[(loop+(N-K))%N];
+                System.out.print(result+" ");
+            }
+            tCount--;
+            System.out.println();
+        }
+    }
+}
